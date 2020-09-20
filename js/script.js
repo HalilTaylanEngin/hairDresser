73bb4efda4235110ed8fa8ef3f49fd85e43fd851
taylan <hlltylnngn@gmail.com>
2020-09-20 14:17:07 +0300

    Ana dizin dosyaları yüklendi

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+$(document).ready(function(){
+
+
+  $("#menuButton").click(function(){
+    $(this).toggleClass("open-toggle-button");
+  });
+
+  $("#menuButton").click(function(){
+    $("#navbarSelf .container").toggleClass("open-navbar");
+  });
+
+  $("#menuButton").click(function(){
+    $(".hero-section-content").toggleClass("d-none");
+  });
+
+
+  // map function
+
+    
+
+
+
+});
